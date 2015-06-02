761e7161cfc46227850a0d1b30bc985a4138a1d4
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-06-02 01:28:05 -0400

    add url formatting function; add all foodservices methods

--- uwapi.go
+++ uwapi.go
@@ -5,11 +5,18 @@ import (
 	"github.com/jeffail/gabs"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const URLPrefix = "https://api.uwaterloo.ca/v2/"
 
-func callAPI(key, url string) (*gabs.Container, error) {
+func callAPI(url string) (*gabs.Container, error) {
+	/*
+		Given the API key and the API endpoint (url), a call is made to the UW
+		API with the url and the key. If any errors occur, an empty
+		gabs.container and the error is returned. Otherwise, the API response
+		is returned as a gabs.container
+	*/
 	var empty *gabs.Container
 
 	// send the get request to the UW API...
@@ -31,9 +38,81 @@ func callAPI(key, url string) (*gabs.Container, error) {
 	}
 
 	return jsonParsed, nil
+}
 
+func formatURL(key string, args ...string) string {
+	argsJoined := strings.Join(args, "/")
+	return fmt.Sprintf("%s%s.json?key=%s", URLPrefix, argsJoined, key)
+}
+
+// FOOD SERVICES ===========================
+type FoodServices struct {
+	key string
+}
+
+func (f FoodServices) Menu() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "menu"))
+	return response, err
+}
+
+func (f FoodServices) Notes() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "notes"))
+	return response, err
+}
+
+func (f FoodServices) Diets() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "diets"))
+	return response, err
+}
+
+func (f FoodServices) Outlets() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "outlets"))
+	return response, err
+}
+
+func (f FoodServices) Locations() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "locations"))
+	return response, err
+}
+
+func (f FoodServices) watcard() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "watcard"))
+	return response, err
+}
+
+func (f FoodServices) announcements() (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "announcements"))
+	return response, err
+}
+
+func (f FoodServices) products(product_ID string) (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", "products", product_ID))
+	return response, err
+}
+
+func (f FoodServices) menu_dated(year, week string) (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", year, week, "menu"))
+	return response, err
+}
+
+func (f FoodServices) notes_dated(year, week string) (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", year, week, "notes"))
+	return response, err
+}
+
+func (f FoodServices) announcements_dated(year, week string) (*gabs.Container, error) {
+	response, err := callAPI(formatURL(f.key, "foodservices", year, week, "announcements"))
+	return response, err
+}
+
+// "wrapper" object
+type UWAPI struct {
+	FoodServices
 }
 
 func main() {
-	fmt.Println("Boop")
+	API_KEY := "YOUR_API_KEY_HERE"
+	uw := UWAPI{
+		FoodServices: FoodServices{key: API_KEY},
+	}
 }
